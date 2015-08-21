dc9379c69fb480776bd4a353e3eb439124235ca5
staser007 <mpm6@yandex.ru>
2015-08-21 17:56:25 +0300

    Новый плагинчик

--- wp-content/plugins/short_pages_url/index.php
+++ wp-content/plugins/short_pages_url/index.php
@@ -31,22 +31,6 @@ function slp_spu_deactivation() {
 
 
 /*
- * Добавляем в админку подстраничку в настройки
- * */
-add_action('admin_menu', 'slp_spu_admin_menu_setup');
-function slp_spu_admin_menu_setup() {
-    add_submenu_page(
-        'options-general.php',
-        'Настройки Short Page URL',
-        'Short PageURL',
-        'manage_options',
-        'slp_shortpageurl',
-        'slp_spu_admin_page_screen'
-    );
-}
-
-
-/*
  * Добавляем ссылку "Настройки" на страницу управления плагинами
  * */
 add_filter('plugin_action_links', 'slp_spu_settings_link', 2, 2);
@@ -59,70 +43,86 @@ function slp_spu_settings_link($actions, $file) {
 
 
 /*
- * Регистрация настроек в системе
+ * Добавляем в админку подстраничку в настройки
  * */
-add_action('admin_init', 'slp_spu_settings_init');
-function slp_spu_settings_init() {
-    register_setting(
-        'slp_spu_options',
-        'slp_spu_options',
-        'slp_spu_options_validate'
-    );
-
-    add_settings_section(
-        'slp_spu_authorbox',
-        'Author\'s box',
-        'slp_spu_authorbox_desc',
-        'slp_shortpageurl'
-    );
-
-    add_settings_field(
-        'slp_shortpageurl_authorbox_template',
-        'Template',
-        'slp_shortpageurl_authorbox_field',
-        'slp_shortpageurl',
-        'slp_spu_authorbox'
+add_action('admin_menu', 'slp_spu_admin_menu_setup');
+function slp_spu_admin_menu_setup() {
+    add_submenu_page(
+        'options-general.php',
+        'Настройки коротких ссылок страниц',
+        'Короткие ссылки',
+        'manage_options',
+        'slpshortpageurl',
+        'slp_spu_admin_page_screen'
     );
 }
 
+
 /*
- * Вывод формы настроек плагина в админке
+ * Вывод подстранички настроек плагина в админке
  * */
 function slp_spu_admin_page_screen() {
     global $submenu;
 
-    // access page settings
     $page_data = array();
-
     foreach ($submenu['options-general.php'] as $i => $menu_item) {
-        if ($submenu['options-general.php'][$i][2] == 'slp_shortpageurl') {
+        if ($submenu['options-general.php'][$i][2] == 'slpshortpageurl') {
             $page_data = $submenu['options-general.php'][$i];
         }
     }
-
-//    var_dump($page_data);
-//    array(4) {
-//      [0]=>string(13) "Short PageURL"
-//      [1]=>string(14) "manage_options"
-//      [2]=>string(16) "slp_shortpageurl"
-//      [3]=>string(33) "Настройки Short Page URL"
-//  }
-
-
-    // output
     ?>
+
     <div class="wrap">
         <?php screen_icon(); ?>
         <h2><?php echo $page_data[3]; ?></h2>
-        <form id="msp_helloworld_options" action="options.php" method="post">
+        <form id="slp_spu_options" action="options.php" method="post">
             <?php
             settings_fields('slp_spu_options');
-            do_settings_sections('slp_shortpageurl');
+            do_settings_sections('slpshortpageurl');
             submit_button('Save options', 'primary', 'msp_helloworld_options_submit');
             ?>
         </form>
     </div>
+
     <?php
+}
+
+
+/*
+ * Регистрация настроек в системе
+ * */
+add_action('admin_init', 'slp_spu_settings_init');
+function slp_spu_settings_init() {
+
+    /* Регистрируем группу настроек */
+    register_setting(
+        'slp_spu_options',          // group
+        'slp_spu_options',          // name
+        'slp_spu_options_validate'  // validate callback
+    );
+
+    /* Добавляем секцию настроек */
+    add_settings_section(
+        'slp_spu_basic_settinngs_section',  // id
+        'Основные',                         // title
+        'slp_spu_basic_desc',               // callback
+        'slpshortpageurl'                   // page
+    );
+    /* Еще одна секция */
+    add_settings_section(
+        'slp_spu_adding_settings_section',  // id
+        'Дополнительно',                    // title
+        'slp_spu_section_desc',             // callback
+        'slpshortpageurl'                   // page
+    );
+
+    add_settings_field(
+        'slp_spu_extension',                    // id
+        'Добавить к name страницы',             // title
+        'slp_spu_extention_field',              // callback
+        'slpshortpageurl',                      // page
+        'slp_spu_basic_settinngs_section'       // section
+    );
 }
 
 
@@ -141,24 +141,32 @@ function slp_spu_options_validate($input) {
 
 
 /*
- * Описание
+ * Описание секции настроек
  * */
-function slp_spu_authorbox_desc() {
-    echo "<p>Enter the template markup for author box using placeholders: [gauthor_name], [gauthor_url], [gauthor_desc] for name, URL and description of author correspondingly.</p>";
+function slp_spu_section_desc() {
+    echo "<p>&nbsp;</p>";
 }
 
 
 /*
- * вывод полей
+ * Вывод полей
  * */
-function slp_shortpageurl_authorbox_field() {
-    $options = get_option('slp_spu_options');
-    $authorbox = (isset($options['authorbox_template'])) ? $options['authorbox_template'] : '';
-    $authorbox = esc_textarea($authorbox);
+function slp_spu_extention1_field() {
+    $options   = get_option('slp_spu_options');
+    $extension = (isset($options['extension'])) ? $options['extension'] : '';
+    $extension = esc_textarea($extension);
     ?>
-    <textarea id="authorbox_template" name="msp_helloworld_options[authorbox_template]" cols="50" rows="5" class="large-text code">
-    <?php echo $authorbox; ?>
+    <textarea id="extension" name="slp_spu_options[extension]" cols="50" rows="5" class="large-text code">
+    <?php echo $extension; ?>
     </textarea>
+    <?php
+}
+function slp_spu_extention_field() {
+    $options   = get_option('slp_spu_options');
+    $extension = (isset($options['extension'])) ? $options['extension'] : '';
+    $extension = esc_html($extension);
+    ?>
+    <input type="text"  id="extension" name="slp_spu_options[extension]" class="code" value="<?php echo $extension; ?>">
     <?php
 }
 
@@ -172,8 +180,29 @@ function slp_spu_page_link_filter($url, $post){
     $post = get_post($post);
     if(!$post || $post->post_type != 'page') return $url;
 
-    return get_option('home') . '/' . $post->post_name . '.html';
+    $options   = get_option('slp_spu_options');
+    $extension = (isset($options['extension'])) ? $options['extension'] : '';
+
+    return get_option('home') . '/' . $post->post_name . $extension;
 }
+
+
+
+add_filter( 'request', 'slp_spu_request_filter');
+function slp_spu_request_filter($query_vars){
+
+    $page = get_posts(array('category_name' => $query_vars['name']));
+
+    var_dump($page);
+//    $query_vars['pagename'] = "structure_car/engine/design_features";
+//    unset($query_vars['name']);
+//    var_dump($query_vars);
+
+    return $query_vars;
+    //var_dump($query_vars); die;
+
+}
+
 
 
 
